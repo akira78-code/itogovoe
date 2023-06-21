029db847c115bb21eb5f9311aadd03a7f9766526
Anton <akira78nl@gmail.com>
2023-06-21 15:43:15 +0500

    Len3_array

--- Program.cs
+++ Program.cs
@@ -15,3 +15,19 @@ string[] Vvod ()
     return vvod_array;
 }
 
+string[] Len3_array()
+{
+    string[] array = Vvod();
+    string new_string = String.Empty;
+    for(int count = 0;count<array.Length;count++)
+    {
+        if(array[count].Length<=3)
+        {
+            new_string=new_string+array[count]+",";
+        }
+    }
+    new_string = new_string.Remove(new_string.Length-1);
+    string[] new_array = new_string.Split(",");
+    return new_array;
+}
+PrintArray(Len3_array());
